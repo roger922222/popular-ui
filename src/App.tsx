346d42ea831692ee2d1c6edd8eb584b5315f672e
luojie08 <m16675184976@163.com>
2020-05-17 12:22:50 +0800

    fix(ui): button组件的完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button onClick={e => alert(e.target)}>hello</Button>
+        <Button disabled>disabled button</Button>
+        <Button size={ButtonSize.Large}>large button</Button>
+        <Button size={ButtonSize.Small}>small button</Button>
+        <Button disabled>hello</Button>
+        <Button btnType={ButtonType.Primary} >primary</Button>
+        <Button btnType={ButtonType.Danger} >danger</Button>
+        <Button btnType={ButtonType.Default} >default</Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com' target="_blank">baidu</Button>
+        <Button btnType={ButtonType.Link} disabled >button disabled</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -22,17 +22,27 @@ interface BaseButtonProps {
     href?: string
 }
 
-const Button: React.FC<BaseButtonProps> = props => {
+// 拿到所有button元素的属性 React.ButtonHTMLAttributes<HTMLElement> 不能通过联合类型去组合 |
+
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps> // Partial a与button标签属性和自定义属性混合在一起，但都是可选的  ？
+
+const Button: React.FC<ButtonProps> = props => {
     const { 
         btnType,
         disabled,
+        className,
         size,
         children,
-        href
+        href,
+        ...restProps
     } = props
 
     // btn, btn-lg, btn-primary
-    const classes = classNames('btn', { 
+    const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
         'disabled': (btnType === ButtonType.Link) && disabled
@@ -43,6 +53,7 @@ const Button: React.FC<BaseButtonProps> = props => {
          <a
             className={classes}
             href={href}
+            { ...restProps }
          >{children}</a>
          )
      } else {
@@ -50,7 +61,7 @@ const Button: React.FC<BaseButtonProps> = props => {
          <button
             className={classes}
             disabled={disabled}
-
+            { ...restProps }
          >{children}</button>
          )
      }
