5486b952eff6f3b360590fe53fbe1f97585bc533
luojie08 <m16675184976@163.com>
2020-05-24 11:04:09 +0800

    fix(ui): button组件的属性由枚举改为字面量类型

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Button from './components/Button/button'
 
 function App() {
   return (
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <Button onClick={e => alert(e.target)}>hello</Button>
         <Button disabled>disabled button</Button>
-        <Button size={ButtonSize.Large}>large button</Button>
-        <Button size={ButtonSize.Small}>small button</Button>
+        <Button size='lg'>large button</Button>
+        <Button size='sm'>small button</Button>
         <Button disabled>hello</Button>
-        <Button btnType={ButtonType.Primary} >primary</Button>
-        <Button btnType={ButtonType.Danger} >danger</Button>
-        <Button btnType={ButtonType.Default} >default</Button>
-        <Button btnType={ButtonType.Link} href='http://www.baidu.com' target="_blank">baidu</Button>
-        <Button btnType={ButtonType.Link} disabled >button disabled</Button>
+        <Button btnType='primary' >primary</Button>
+        <Button btnType='danger' >danger</Button>
+        <Button btnType='default' >default</Button>
+        <Button btnType='link' href='http://www.baidu.com' target="_blank">baidu</Button>
+        <Button btnType='link' disabled >button disabled</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import classNames from 'classnames'
 
-export enum ButtonSize {
-    Large = 'lg',
-    Small = 'sm'
-}
+export type ButtonSize = 'lg' | 'sm'
 
-export enum ButtonType {
-    Primary = 'primary',
-    Default = 'default',
-    Danger = 'danger',
-    Link = 'link'
-}
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
     className?: string;
@@ -45,10 +37,10 @@ const Button: React.FC<ButtonProps> = props => {
     const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
-        'disabled': (btnType === ButtonType.Link) && disabled
+        'disabled': (btnType === 'link') && disabled
      })
 
-     if (btnType === ButtonType.Link && href) {
+     if (btnType === 'link' && href) {
          return (
          <a
             className={classes}
@@ -69,7 +61,7 @@ const Button: React.FC<ButtonProps> = props => {
 
 Button.defaultProps = {
     disabled: false,
-    btnType: ButtonType.Default
+    btnType: 'default'
 }
 
 export default Button
